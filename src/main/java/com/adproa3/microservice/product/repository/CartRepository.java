01ac3bb5822281be39033c012fc947dc80c5d78a
Abbilville <abbilville@gmail.com>
2024-04-28 17:41:56 +0700

    deploy

---
+++ src/main/java/com/adproa3/microservice/product/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.adproa3.microservice.product.repository;
+
+import com.adproa3.microservice.product.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class CartRepository {
+}
