7bcbfcad228020b49ec43fc6677e2a310d9e9230
Abbilville <abbilville@gmail.com>
2024-05-01 19:33:35 +0700

    [GREEN] Implement Cart service interface

--- src/main/java/com/adproa3/microservice/product/service/CartService.java
+++ src/main/java/com/adproa3/microservice/product/service/CartService.java
@@ -1,8 +1,12 @@
 package com.adproa3.microservice.product.service;
 
-import com.adproa3.microservice.product.model.Product;
 import com.adproa3.microservice.product.model.Cart;
-import java.util.List;
+import com.adproa3.microservice.product.model.Product;
+
 
 public interface CartService {
+    public Cart create(Cart cart);
+    public Cart findCartById(String cartId);
+    public Cart addProductToCart(String cartId, Product product, int quantity);
+    public Cart removeProductFromCart(String cartId, Product product);
 }
