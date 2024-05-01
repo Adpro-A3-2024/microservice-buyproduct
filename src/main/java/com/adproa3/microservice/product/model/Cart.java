2d89ea690747587d79ab1be9a4b769be3c57ebe0
Abbilville <abbilville@gmail.com>
2024-05-01 19:32:42 +0700

    [GREEN] Implement Cart model

--- src/main/java/com/adproa3/microservice/product/model/Cart.java
+++ src/main/java/com/adproa3/microservice/product/model/Cart.java
@@ -1,12 +1,23 @@
 package com.adproa3.microservice.product.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import java.util.List;
 
-@Getter @Setter
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @Setter @Data
 public class Cart {
     private String cartId;
     private String userId;
-    private List<Product> products;
+    private Map<Product, Integer> products;
+    private double totalPrice;
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public Cart(String userId) {
+        this.userId = userId;
+        this.products = new HashMap();
+        this.totalPrice = 0;
+    }
 }
