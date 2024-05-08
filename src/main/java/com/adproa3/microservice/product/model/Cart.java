16d0880eaedaf0effba4f4ed56eeae1b40834755
Abbilville <abbilville@gmail.com>
2024-05-08 19:55:57 +0700

    [GREEN] Implement Cart Model

--- src/main/java/com/adproa3/microservice/product/model/Cart.java
+++ src/main/java/com/adproa3/microservice/product/model/Cart.java
@@ -4,20 +4,38 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Getter @Setter @Data
+@Entity
+@Table(name = "carts")
 public class Cart {
-    private String cartId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long internalId;
+
+    private UUID cartId;
     private String userId;
-    private Map<Product, Integer> products;
     private double totalPrice;
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @ElementCollection
+    @CollectionTable(name = "cart_items", joinColumns = @JoinColumn(name = "cart_id"))
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "quantity")
+    private Map<String, Integer> productsInCart = new HashMap<>();
+
     public Cart(String userId) {
         this.userId = userId;
-        this.products = new HashMap();
-        this.totalPrice = 0;
+        generateId();
+    }
+
+    @PrePersist
+    public void generateId() {
+        if (this.cartId == null) {
+            this.cartId = UUID.randomUUID();
+        }
     }
 }
