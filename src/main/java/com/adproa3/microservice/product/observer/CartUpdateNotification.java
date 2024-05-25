f9d19970fdb3885bad78fcfc949473a2d2c6b3c8
Abbilville <abbilville@gmail.com>
2024-05-25 21:23:29 +0700

    [GREEN] Implement Cart update notification

--- src/main/java/com/adproa3/microservice/product/observer/CartUpdateNotification.java
+++ src/main/java/com/adproa3/microservice/product/observer/CartUpdateNotification.java
@@ -1,11 +1,15 @@
 package com.adproa3.microservice.product.observer;
 
 import com.adproa3.microservice.product.model.Cart;
+import com.adproa3.microservice.product.model.tempModel.Order;
 
 public class CartUpdateNotification implements Observer {
-    @Override
-    public void update(Cart cart) {
-        // Kirim ke pengguna
-        System.out.println("Keranjang belanja telah diperbarui. Total harga: " + cart.getTotalPrice());
+    public void update(Object object) {
+        if (object instanceof Cart cart) {
+            System.out.println("Keranjang belanja telah diperbarui. Total harga: " + cart.getTotalPrice());
+        }
+        if (object instanceof Order order) {
+            System.out.println("Order telah dibuat. Total harga: " + order.getTotalPrice());
+        }
     }
 }
