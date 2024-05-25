e8c83384e340d8b00c95dbee851c7e32ea33af50
Abbilville <abbilville@gmail.com>
2024-05-25 21:22:23 +0700

    [GREEN] Implement Cart observable

--- src/main/java/com/adproa3/microservice/product/observable/CartObservable.java
+++ src/main/java/com/adproa3/microservice/product/observable/CartObservable.java
@@ -1,6 +1,7 @@
 package com.adproa3.microservice.product.observable;
 
 import com.adproa3.microservice.product.model.Cart;
+import com.adproa3.microservice.product.model.tempModel.Order;
 import com.adproa3.microservice.product.observer.Observer;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +23,12 @@ public class CartObservable {
     public void notifyObservers(Cart cart) {
         for (Observer observer : observers) {
             observer.update(cart);
+        }
+    }
+
+    public void notifyObservers(Order order) {
+        for (Observer observer : observers) {
+            observer.update(order);
         }
     }
 }
